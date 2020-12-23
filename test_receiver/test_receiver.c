5af364a4b074faf17ec6097a4ac1cefe7acebeb1
Tobias Gierke <tobias.gierke@code-sourcery.de>
2020-12-23 17:56:50 +0100

    wip

--- test_receiver/test_receiver.c
+++ test_receiver/test_receiver.c
@@ -1,13 +1,22 @@
-#include "radio_common.h"
 #include "radio_receiver.h"
+#include "radio_common.h"
 #include "uart.h"
 
 void main(void) {
 
-  radio_init();
+  uint8_t buffer[MSG_LEN];
+  radio_receiver_init();
   uart_init();
 
   while(1) {
-  
-  }
+		uint8_t received_bytes = radio_receive(&buffer[0],MSG_LEN);
+		if ( ( received_bytes & 0x80 ) == 0 && received_bytes > 0 ) {
+			for ( uint8_t i = 0 ; i < received_bytes ; i++ ) {
+				uart_print("\r\nByte ");
+				uart_putsdecimal( i );
+				uart_print(" : ");
+				uart_putsdecimal( buffer[i] );
+			}
+		}
+	}
 }
