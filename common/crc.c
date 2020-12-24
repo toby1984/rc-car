f89c7b3df4639fa9d9a9b9435d3c165d16b2031d
Tobias Gierke <tobias.gierke@code-sourcery.de>
2020-12-24 01:30:22 +0100

    wip

--- common/crc.c
+++ common/crc.c
@@ -1,40 +1,13 @@
 #include "crc.h"
 
 uint8_t crc8(uint8_t *data,uint8_t dataLen) {
-    uint8_t shiftReg = 0;
-    for ( uint8_t idx = 0 ; idx < dataLen; idx++ ) {
-        uint8_t currentByte = *data++;
-        for ( int8_t bit = 7 ; bit >= 0 ; bit--, currentByte <<= 1 ) {
-
-            uint8_t bit0 = (shiftReg & 0x01) != 0 ? 1:0;
-            uint8_t bit1 = (shiftReg & 0x02) != 0 ? 1:0;
-            uint8_t bit2 = (shiftReg & 0x04) != 0 ? 1:0;
-            uint8_t bit7 = (shiftReg & 0x80) != 0 ? 1:0;
-
-            shiftReg <<= 1;
-            if ( (currentByte & 0x80) != 0 ) {
-                shiftReg |= 1;
-            }
-
-            // bit1 = bit0 ^ bit7
-            if ( bit0 ^ bit7 ) {
-                shiftReg |= 0x02;
-            } else {
-                shiftReg &= ~0x02;
-            }
-            // bit2 = bit1 ^ bit7
-            if ( bit1 ^ bit7 ) {
-                shiftReg |= 0x04;
-            } else {
-                shiftReg &= ~0x04;
-            }
-            // bit3 = bit2 ^ bit7
-            if ( bit2 ^ bit7 ) {
-                shiftReg |= 0x08;
-            } else {
-                shiftReg &= ~0x08;
+        uint8_t crc = 0xff;
+        for (uint8_t j = 0; j < dataLen; j++)
+        {
+            crc ^= data[j];
+            for (uint8_t i = 0; i < 8; i++) {
+                crc = (uint8_t) ((crc>>1) ^ ((0-(crc&1))&0xf5));
             }
         }
-    }
-    return shiftReg;
+        return crc;
 }
